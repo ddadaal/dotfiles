54ba0e51610e63d45ec3082ac3c9b0c7cb013cad
Chen Junda <ddadaal@outlook.com>
2020-04-06 14:52:16 +0800

    Add p10k theme in init zsh

--- init-zsh.sh
+++ init-zsh.sh
@@ -29,6 +29,9 @@ git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zs
 echo "Install zsh-syntax-highlighting..."
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+echo "Install p10k theme..."
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
 # Copy zshrc
 
 echo "Backup existing .zshrc..."
