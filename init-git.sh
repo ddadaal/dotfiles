aaf78198ee9cdf36a52714c00ee7155cc8be5588
ddadaal <ddadaal@outlook.com>
2020-04-07 10:07:03 +0800

    add proxy and remove autocrlf in init-git

--- init-git.sh
+++ init-git.sh
@@ -1,6 +1,6 @@
 git config --global user.name "ddadaal"
 git config --global user.email "ddadaal@outlook.com"
-git config --global core.autocrlf false
 git config --global credenetials.helper store
+git config --global http.proxy "http://127.0.0.1:1080"
 
 
