6eabe23ed9919024c45514cadaf212caa2c1dc58
Chen Junda <ddadaal@outlook.com>
2020-06-19 10:43:10 +0800

    use fcitx instead of ibus

--- init-desktop.sh
+++ init-desktop.sh
@@ -6,22 +6,26 @@
 echo "Install fonts..."
 sudo pacman -S adobe-source-code-pro-fonts wqy-bitmapfont wqy-microhei wqy-zenhei adobe-source-han-sans-cn-fonts ttf-font-awesome ttf-dejavu nerd-fonts-complete noto-fonts-cjk --needed
 
-# echo "Install fcitx and input methods..."
-# sudo pacman -S fcitx fcitx-configtool fcitx-im fcitx-rime --needed
+echo "Install fcitx and input methods..."
+sudo pacman -S fcitx fcitx-configtool fcitx-im fcitx-rime --needed
 
-# echo "To configure fcitx, copy .xprofile to ~ on X, or copy the contents of .xprofile to /etc/environments on wayland"
+echo "To configure fcitx, copy .xprofile to ~ on X, or copy the contents of .xprofile to /etc/environments on wayland"
+echo "If on GNOME, install https://extensions.gnome.org/extension/261/kimpanel/ for better desktop environment integration"
+echo "Now entering fcitx-config-gtk3."
+fcitx-config-gtk3
 
-echo "Install ibus and rime"
-sudo pacman -S ibus-rime ibus --needed
 
-echo "Setup autostart"
-cp ./ibus-daemon.desktop ~/.config/autostart/
+# echo "Install ibus and rime"
+# sudo pacman -S ibus-rime ibus --needed
 
-echo "Running ibus-daemon"
-nohuo ibus-daemon --xim & disown
+# echo "Setup autostart"
+# cp ./ibus-daemon.desktop ~/.config/autostart/
 
-echo "Running ibus-setup"
-ibus-setup
+# echo "Running ibus-daemon"
+# nohuo ibus-daemon --xim & disown
 
-echo "Add the Rime IME in Settings - Region & Language - Input Sources"
+# echo "Running ibus-setup"
+# ibus-setup
+
+# echo "Add the Rime IME in Settings - Region & Language - Input Sources"
 
