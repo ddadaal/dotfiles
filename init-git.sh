4ec2dee1a62532f807aac5326ca3c9f78ce3ec73
Chen Junda <ddadaal@outlook.com>
2020-06-21 17:20:24 +0800

    change a typo at init-git

--- init-git.sh
+++ init-git.sh
@@ -1,6 +1,6 @@
 git config --global user.name "ddadaal"
 git config --global user.email "ddadaal@outlook.com"
-git config --global credenetials.helper store
+git config --global credentials.helper store
 git config --global http.proxy "http://127.0.0.1:1080"
 
 
