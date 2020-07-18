f1b908dbb15a4bed29052d93bf867aa9e812f086
Chen Junda <ddadaal@outlook.com>
2020-07-18 11:41:54 +0800

    improve powershell PROFILE

--- init-powershell.ps1
+++ init-powershell.ps1
@@ -11,4 +11,14 @@ Add-Content -Path $PROFILE -Value @"
 Import-Module posh-git
 Import-Module oh-my-posh
 Set-Theme $THEME
+
+function proxify {
+        $env:HTTP_PROXY="http://127.0.0.1:1080"
+        $env:HTTPS_PROXY="http://127.0.0.1:1080"
+}
+
+function unproxify {
+        $env:HTTP_PROXY=$null
+        $env:HTTPS_PROXY=$null
+}
 "@
