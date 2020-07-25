e347aee5f36d01ad704e21f837c7147d14ac6799
Chen Junda <ddadaal@outlook.com>
2020-07-25 11:36:36 +0800

    Update init-wsl2-proxy-forwarding.sh

--- init-wsl2-proxy-forwarding.sh
+++ init-wsl2-proxy-forwarding.sh
@@ -12,7 +12,7 @@ sudo sed -i 's/#Listen 192.168.0.1/Listen 127.0.0.1/' $CONFIG_PATH
 
 # Set the upstream to Windows proxy
 # Windows's hostname is good
-sudo sed -i 's/#Upstream http some.remote.proxy:port/Upstream host.docker.internal/' $CONFIG_PATH
+sudo sed -i 's/#Upstream http some.remote.proxy:port/Upstream http host.docker.internal:1080/' $CONFIG_PATH
 
 # Run the tinyproxy
 tinyproxy
