406dce28e0321b75494253ceecd2ea6695ebbcc3
ddadaal <ddadaal@outlook.com>
2021-03-22 15:36:43 +0800

    use antigen for zsh conf

--- init-zsh.sh
+++ init-zsh.sh
@@ -9,35 +9,18 @@ fi
 # Install git and zsh
 sudo pacman -S git zsh --needed
 
-# Install oh-my-zsh
-if ! [ -d ~/.oh-my-zsh ]; then
-    echo "Install oh-my-zsh..."
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
-    echo "Change default shell to zsh..."
-    chsh -s /bin/zsh
-fi
-# Install plugins
+# Install antigen
+paru -S antigen
 
-echo "Install plugins..."
-
-echo "Install zsh-autosuggestions..."
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-echo "Install zsh-completions..."
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
-
-echo "Install zsh-syntax-highlighting..."
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-echo "Install p10k theme..."
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
 # Copy zshrc
-
 echo "Backup existing .zshrc..."
 mv ~/.zshrc ~/.zshrc.backup
+
 echo "Copy .zshrc"
 cp .zshrc ~
 
 # Copy p10k conf
 cp .p10k.zsh ~/
+
+echo "Run zsh to init"
